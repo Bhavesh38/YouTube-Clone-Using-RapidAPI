6f0934beb861bc333e25f5678851bc0b5b4fd0a8
Bhavesh <bhavesh953475@gmail.com>
2023-01-10 14:12:26 +0530

    Channel Details added

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,6 +1,19 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Videos from "./Videos";
+import ChannelCard from './ChannelCard';
+import { fetchFromAPI } from "../utils/fetchFromAPI"
 const ChannelDetail = () => {
+    const [ChannelDetail, setChannelDetail] = useState(null);
+    const [videos, setVideos] = useState([]);
+    const { id } = useParams();
+    console.log(ChannelDetail, videos);
+    useEffect(() => {
+        fetchFromAPI(`channels?.part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+
+        fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items))
+    }, [id])
     return (
         <div>
             ChannelDetail
