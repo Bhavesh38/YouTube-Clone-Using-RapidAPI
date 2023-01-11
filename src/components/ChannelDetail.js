5bcc2f48b959ad8fa53d88f2b24f8cbe5a0983a5
Bhavesh <bhavesh953475@gmail.com>
2023-01-11 22:35:12 +0530

    video player added

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -5,20 +5,31 @@ import Videos from "./Videos";
 import ChannelCard from './ChannelCard';
 import { fetchFromAPI } from "../utils/fetchFromAPI"
 const ChannelDetail = () => {
-    const [ChannelDetail, setChannelDetail] = useState(null);
+    const [channelDetail, setChannelDetail] = useState(null);
     const [videos, setVideos] = useState([]);
     const { id } = useParams();
-    console.log(ChannelDetail, videos);
+    console.log(channelDetail, videos);
     useEffect(() => {
         fetchFromAPI(`channels?.part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
 
         fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items))
     }, [id])
     return (
-        <div>
-            ChannelDetail
-        </div>
+        <Box>
+            <Box>
+                <div style={{
+                    background: "linear-gradient(90deg, rgba(0, 238, 247, 1) 0%, rgba(206, 3, 184, 1) 100%, rgba(255, 0, 139, 1) 100%)",
+                    height: "300px",
+                    zIndex: 10
+                }} />
+                <ChannelCard channelDetail={channelDetail} marginTop="-100px" />
+            </Box>
+            <Box display="flex" p="2">
+                <Box sx={{ mr: { sm: "100px" } }} />
+                <Videos videos={videos} />
+            </Box>
+        </Box>
     )
 }
 
-export default ChannelDetail
+export default ChannelDetail;
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
+    const { id } = useParams();
+    const [videoDetail, setVideoDetail] = useState(null);
+
+    useEffect(() => {
+        fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.items[0]));
+    }, [id]);
+    console.log(videoDetail);
+    if (!videoDetail?.snippet) return <h1>Loading</h1>;
+    const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = VideoDetail;
     return (
-        <div>
-            videodetails
-        </div>
+        <Box minHeight="95vh">
+            <Stack direction={{ xs: "column", md: "row" }}>
+                <Box flex={1}>
+                    <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+                        <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+                        <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+                            {title}
+                        </Typography>
+                        {/* <Stack>
+
+                        </Stack> */}
+                    </Box>
+                </Box>
+            </Stack>
+        </Box>
     )
 }
 
